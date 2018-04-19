04b3e601a7de049365e5000b30b7ff4e49ea9f6c
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2018-04-19 20:14:24 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -21,7 +21,7 @@ class RecipesController < ApplicationController
 
   def recipe_params
     params.require(:recipe).permit(
-      :name,
+      :title,
       ingredients_attributes: [
         :name,
         :quantity
